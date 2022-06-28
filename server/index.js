d1cb3c55df4d889e71abfce89c1acb0455d636c3
JJooaa <leitola.joa@gmail.com>
2022-06-28 09:09:07 +0300

    remove unnecessary http server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const app = require("./app");
-const http = require("http");
 
-const server = http.createServer(app);
-
-server.listen("3001", () => {
+app.listen("3001", () => {
   console.log("Server running on port 3001");
 });
